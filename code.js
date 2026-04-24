9e7287d6b7d26359208c39dc67b8cf752a88eb39
pedrim-hc <145812205+pedrim-hc@users.noreply.github.com>
2026-04-24 09:34:02 -0300

    pronto

---
+++ code.js
@@ -0,0 +1,8 @@
+let resposta = document.getElementById("resposta")
+let n1 = document.getElementById("n1")
+let n2 = document.getElementById("n2")
+
+function soma(){
+    let conta = n1 + n2
+    resposta.innerHTML(soma)
+}
