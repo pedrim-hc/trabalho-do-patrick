489005e8fa1d35dbc7f587734bd8374fcce1e480
pedrim-hc <145812205+pedrim-hc@users.noreply.github.com>
2026-04-25 08:06:54 -0300

    teste

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 let resposta = document.getElementById("resposta")
 
 function trans(){
-    let temp = Number(document.getElementById("temp").value)
+    let n1 = Number(document.getElementById("n1").value)
     let conta = 0
 
-    conta = (temp * 9/5) + 32 
+    conta = (n1 * 9/5) + 32 
     resposta.innerHTML = "A conversão para fahrenheit é "+conta
 }
