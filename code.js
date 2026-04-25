94a6901a89a300d009d51a5d4063e255fc6d54ac
pedrim-hc <145812205+pedrim-hc@users.noreply.github.com>
2026-04-25 08:39:33 -0300

    j

--- code.js
+++ code.js
@@ -1,23 +1,31 @@
+
 let resposta = document.getElementById("resposta")
-   
 function soma(){
+
     let n1 = Number(document.getElementById("n1").value)
     let n2 = Number(document.getElementById("n2").value)
+    
     resposta.innerHTML = n1 + n2
 }
 function sub(){
+      
     let n1 = Number(document.getElementById("n1").value)
     let n2 = Number(document.getElementById("n2").value)
+      
     resposta.innerHTML = n1 - n2
     
 }
 function multi(){
+      //Colocando os valores dados pelo usuario
     let n1 = Number(document.getElementById("n1").value)
     let n2 = Number(document.getElementById("n2").value)
+      
     resposta.innerHTML = n1 * n2
 }
 function div(){
+      //Colocando os valores dados pelo usuario
     let n1 = Number(document.getElementById("n1").value)
     let n2 = Number(document.getElementById("n2").value)
+      
     resposta.innerHTML = n1 / n2
 }
