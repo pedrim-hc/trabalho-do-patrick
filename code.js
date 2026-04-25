3d7a5221999fc92c9268a210deb04188b6451414
pedrim-hc <145812205+pedrim-hc@users.noreply.github.com>
2026-04-25 08:29:09 -0300

    pronto

--- code.js
+++ code.js
@@ -3,21 +3,21 @@ let resposta = document.getElementById("resposta")
 function soma(){
     let n1 = Number(document.getElementById("n1").value)
     let n2 = Number(document.getElementById("n2").value)
-    resposta.innerHTML = n1+n2
+    resposta.innerHTML = n1 + n2
 }
 function sub(){
     let n1 = Number(document.getElementById("n1").value)
     let n2 = Number(document.getElementById("n2").value)
-    resposta.innerHTML = n1-n2
+    resposta.innerHTML = n1 - n2
     
 }
 function multi(){
     let n1 = Number(document.getElementById("n1").value)
     let n2 = Number(document.getElementById("n2").value)
-    resposta.innerHTML = n1*n2
+    resposta.innerHTML = n1 * n2
 }
 function div(){
     let n1 = Number(document.getElementById("n1").value)
     let n2 = Number(document.getElementById("n2").value)
-    resposta.innerHTML = n1/n2
+    resposta.innerHTML = n1 / n2
 }
