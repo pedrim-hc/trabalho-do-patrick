aa0f2b186c6e6b50143cf84354709c35a6dc4f32
pedrim-hc <145812205+pedrim-hc@users.noreply.github.com>
2026-04-25 08:15:47 -0300

    h

--- code.js
+++ code.js
@@ -1,10 +1,10 @@
 let resposta = document.getElementById("resposta")
     let n1 = Number(document.getElementById("n1").value)
     let n2 = Number(document.getElementById("n2").value)
-
+    let conta = 0
 function soma(){
 
-    let conta = n1 + n2
+    conta == n1 + n2
     resposta.innerHTML = conta
 }
 function sub(){
