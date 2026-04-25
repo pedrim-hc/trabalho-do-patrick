771082f8de856ed5dddc861626b03887a1a81006
pedrim-hc <145812205+pedrim-hc@users.noreply.github.com>
2026-04-25 07:59:27 -0300

    2

--- code.js
+++ code.js
@@ -3,22 +3,22 @@ let n1 = Number(document.getElementById("n1").value)
 let n2 = Number(document.getElementById("n2").value)
 
 function soma(){
-    let conta
+    let conta = 0
     conta = n1 + n2
     resposta.innerHTML(conta)
 }
 function sub(){
-    let conta
+    let conta = 0
     conta = n1 + n2
     resposta.innerHTML(conta)
 }
 function multi(){
-    let conta
+    let conta = 0
     conta = n1 + n2
     resposta.innerHTML(conta)
 }
 function div(){
-    let conta
+    let conta = 0
     conta = n1 + n2
     resposta.innerHTML(conta)
 }
