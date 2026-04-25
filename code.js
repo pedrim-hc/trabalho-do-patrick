ea11cc921d59f7ad4ac33e420a5c1cd370bcf912
pedrim-hc <145812205+pedrim-hc@users.noreply.github.com>
2026-04-25 08:22:12 -0300

    12

--- code.js
+++ code.js
@@ -1,11 +1,9 @@
 let resposta = document.getElementById("resposta")
    
 function soma(){
-     let n1 = Number(document.getElementById("n1").value)
+    let n1 = Number(document.getElementById("n1").value)
     let n2 = Number(document.getElementById("n2").value)
-    let conta = 0
-    conta = n1+n2
-    resposta.innerHTML = conta
+    resposta.innerHTML = n1+n2
 }
 function sub(){
     let n1 = Number(document.getElementById("n1").value)
